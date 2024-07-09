f113b97dc0ccadcd1ccef17e25759a6ca2a879e3
김도영 <zz01064842675@gmail.com>
2024-07-09 17:40:47 +0900

    test

--- src/main/java/com/example/batch/service/webhook/api/dto/WebhookVO.java
+++ src/main/java/com/example/batch/service/webhook/api/dto/WebhookVO.java
@@ -6,15 +6,12 @@ import lombok.ToString;
 @Data
 @ToString
 public class WebhookVO {
-    private String channelId;
-    private String channelName;
-    private String teamDomain;
-    private String teamId;
-    private String postId;
     private String text;
-    private String timestamp;
-    private String token;
-    private String triggerWord;
-    private String userId;
-    private String userName;
+    private String responseType;
+    private String username;
+    private String iconUrl;
+    private String attachments;
+    private String type;
+    private String props;
+    private String priority;
 }
