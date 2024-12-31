ee90aacab05ef308d89bf5637daf3c81d54d81b6
김도영 <zz01064842675@gmail.com>
2024-12-31 17:09:51 +0900

    reset try catch

--- src/main/java/com/example/batch/service/reset/database/rep/jpa/ResetPointREP.java
+++ src/main/java/com/example/batch/service/reset/database/rep/jpa/ResetPointREP.java
@@ -2,10 +2,12 @@ package com.example.batch.service.reset.database.rep.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
 
+@Repository
 public interface ResetPointREP extends JpaRepository<ResetPointEntity, Long> {
     List<ResetPointEntity> findByResetYnAndPointIdInOrderByCreateDateDesc(@NonNull String resetYn, @NonNull Collection<Integer> pointIds);
 }
