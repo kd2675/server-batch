6f9053dbf8c75087dc1bbee27930fc540ada9e89
김도영 <zz01064842675@gmail.com>
2025-01-06 08:43:33 +0900

    mattermost 재생성

--- src/main/java/com/example/batch/utils/enums/ChannelEnum.java
+++ src/main/java/com/example/batch/utils/enums/ChannelEnum.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ChannelEnum {
 
-    MATTERMOST_CHANNEL_BOB(5L, "bob", "ao4inqs76jf4bqsw3ukyj84tyw"),
-    MATTERMOST_CHANNEL_BOT(5L, "bob", "py5k9sbz1f8udfa5fgj9abcjsw"),
+    MATTERMOST_CHANNEL_BOB(5L, "bob", "jsz1cxtjwfb63ccx35bpt9c3nh"),
+    MATTERMOST_CHANNEL_BOT(5L, "bob", "pdxfh1a7jfg1dm8x4pbdyzrhta"),
 
     MATTERMOST_CHANNEL_COIN(0L, "coin", "49te3so5pirzubzrwis4h39uya"),
     MATTERMOST_CHANNEL_NEWS(1L, "news", "7ah4awp48fd9dfqxs87tia4h6c"),
