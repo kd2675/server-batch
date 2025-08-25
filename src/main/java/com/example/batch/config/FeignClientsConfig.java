1394428065be111a9420d5852a10ba9a5ef4c7c1
김도영 <zz01064842675@gmail.com>
2025-08-25 18:05:09 +0900

    로드밸런싱 및 fallback 처리중

--- src/main/java/com/example/batch/config/FeignClientsConfig.java
+++ src/main/java/com/example/batch/config/FeignClientsConfig.java
@@ -6,7 +6,6 @@ import feign.RequestInterceptor;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
