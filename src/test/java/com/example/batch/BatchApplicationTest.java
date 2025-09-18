5dc11e7e54c476ad1507306745288e4b007efa2a
김도영 <zz01064842675@gmail.com>
2025-09-18 10:29:20 +0900

    test코드 수정

---
+++ src/test/java/com/example/batch/BatchApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BatchApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
